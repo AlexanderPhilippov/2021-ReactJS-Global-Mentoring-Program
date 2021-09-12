54680d8e84ea819fd2cd972a3158b705c6bc4b4d
Alexander Philippov <alexphil@gmail.com>
2021-09-12 20:40:51 +0300

    Add simple components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Component, PureComponent, FunctionalComponent, ReactElement } from './Components/TaskOneComponents';
 
 
 const App = () => {
   return (
     <div>
-    Hello world!
-  </div>
+      <Component componentName = "React.Component"/>
+      <PureComponent />
+      <FunctionalComponent />
+      <ReactElement />
+    </div>
   )
 }
 
---
+++ src/Components/TaskOneComponents/index.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import './index.css';
+
+export class Component extends React.Component<{componentName: string}>{
+    render(){
+        return(
+            <div>Hello from {this.props.componentName}</div>
+        )
+    }
+}
+
+
+export class PureComponent extends React.PureComponent{
+    render(){
+        return(
+            <div>Hello from React.PureComponent</div>
+        )
+    }
+}
+
+export const FunctionalComponent: React.FC = () => {
+    const [counter, setCounter] = useState(0)
+    
+    const handleChangeCounter = (e: React.SyntheticEvent, value: number) => {
+        setCounter(value)
+    }
+
+    return (
+        <div className="counter">
+            <div>Hello from React.FC</div>
+            <div>Current counter value is:<span className="counter__value">{counter}</span></div>
+            <div>
+                <button className="counter_button" onClick={(e) => handleChangeCounter(e, counter-1)}>-</button>
+                <button className="counter_button" onClick={(e) => handleChangeCounter(e, counter+1)}>+</button>
+            </div>
+            <div className="counter__info">To change value click on particular button</div>
+        </div>
+        
+    )
+}
+
+export const ReactElement = () => {
+    return React.createElement('div', null, 'Hello from React.createElement')
+}
