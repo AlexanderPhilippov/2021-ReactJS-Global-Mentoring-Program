54743d276358e82d556b82a652f5343f96a66857
Alexander Philippov <alexphil@gmail.com>
2021-09-20 11:12:44 +0300

    Homework Task 2 (#4)
    * Added prod and dev webpack configuration
    
    * Added tsconfig
    
    Removed redundant code

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SimpleCounter from 'Components/SimpleCounter';
+
+const App: React.FC<{}> = () => {
+  return (
+    <div className='main'>
+      <SimpleCounter />
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    context: path.join(__dirname, 'src'),
+    entry: {
+        main: ['@babel/polyfill', 'index.tsx']
+    },
+    output: {
+        filename: isProd ? '[name].[fullhash].js' : '[name].js',
+        path: path.join(__dirname, 'dist')
+    },
+    resolve: {
+        modules: [path.join(__dirname, 'src'), 'node_modules'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+        alias: {
+            Components: path.resolve(__dirname, 'src/Components')
+        }
+    },
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx|ts|tsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+                        }
+                    }
+                ]
+            }, 
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    {loader: MiniCssExtractPlugin.loader},
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|svg|gif|ttf|woff|woff2|eot)/,
+                use: ['file-loader']
+            }
+        ]
+    },
+    devtool: isDev ? 'source-map' : false,
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: isProd ? '[name].[fullhash].css' : '[name].css',
+            chunkFilename: '[name].[chunkhash].css'
+        }),
+    ],
+    optimization:{
+        minimizer: [
+            new OptimizeCssAssetWebpackPlugin(),
+            new TerserWebpackPlugin({
+                extractComments: false
+            })
+        ],
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    performance: {
+        hints: isProd ? 'error' : false,
+    },
+    devServer: {
+        port: 3000
+    }
+}
