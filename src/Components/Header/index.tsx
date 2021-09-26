6aa5671ae3e182f95ca97f856e3caaafe082408b
Alexander Philippov <alexphil@gmail.com>
2021-09-27 02:20:01 +0300

    Worning with header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import image from 'Assets/Images/movies-tiles.jpg'
+import './styles.scss'
 
 const Header: React.FC = () => {
     return (
-        <>
-            <div>It is Header</div>
-        </>
+        <div className="header">
+            <img className="header__image" src={image} />
+            <div className="header__text-logo">
+                <span>netflix</span>roulette
+            </div>
+            <input className="header__add-movie_button" type="button" value="+ add movie" />
+            <div className="header__search">
+                <label htmlFor="searchMovieInput">Find your movie</label>
+                <input id="searchMovieInput" type="text" placeholder="What do you want to watch?" />
+                <input type="button" value="search" />
+            </div>
+        </div>
     )
 }
 
