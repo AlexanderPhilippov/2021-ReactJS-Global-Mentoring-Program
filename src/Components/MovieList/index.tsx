37e5ad0101132a7c7d67dd4ca860b15e8bfb7294
Alexander Philippov <alexphil@gmail.com>
2021-09-27 19:39:08 +0300

    Changes according reccomendations

--- src/Components/MovieList/index.tsx
+++ src/Components/MovieList/index.tsx
@@ -9,8 +9,8 @@ const MovieList: React.FC = () => {
     return (
         <div className="movie-list">
             <MoviesHeader total={MoviesMockData.totalAmount}/>
-            {(MoviesMockData as MoviesResponseModel)?.data.map(x => (
-                <Movie key={x.id} movie={x} />
+            {(MoviesMockData as MoviesResponseModel)?.data.map(movie => (
+                <Movie key={movie.id} movie={movie} />
             ))}
         </div>
     )
