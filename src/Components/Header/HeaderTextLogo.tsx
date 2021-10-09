4a03b8c31e8ad4c39fe1d825ddd44334cc70efc9
Alexander Philippov <alexphil@gmail.com>
2021-10-09 19:01:21 +0300

    Extract component

---
+++ src/Components/Header/HeaderTextLogo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HeaderTextLogo: React.FC = () => {
+    return (
+        <div className="header__text-logo">
+            <span>netflix</span>roulette
+        </div>
+    )
+}
+
+export default HeaderTextLogo
--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,9 +1,9 @@
 import MovieForm from 'Components/MovieForm'
 import { Modal } from 'Components'
-import image from 'Assets/Images/movies-tiles.jpg'
 import React, { useState } from 'react'
 import { MovieFormAction } from 'Components/MovieForm/models'
 import './styles.scss'
+import SearchPanel from './SearchPanel'
 
 const Header: React.FC = () => {
     const [isModalOpen, setIsModalOpen] = useState(false)
@@ -17,25 +17,7 @@ const Header: React.FC = () => {
                 <MovieForm action={MovieFormAction.ADD} />
             </Modal>
             <div className="header">
-                <img className="header__image" src={image} />
-                <div className="header__text-logo">
-                    <span>netflix</span>roulette
-                </div>
-                <input
-                    className="header__add-movie_button"
-                    type="button"
-                    value="+ add movie"
-                    onClick={() => setIsModalOpen((prevState) => !prevState)}
-                />
-                <div className="header__search">
-                    <label htmlFor="searchMovieInput">Find your movie</label>
-                    <input
-                        id="searchMovieInput"
-                        type="text"
-                        placeholder="What do you want to watch?"
-                    />
-                    <input type="button" value="search" />
-                </div>
+                <SearchPanel modalToggle={setIsModalOpen}/>
             </div>
         </>
     )
