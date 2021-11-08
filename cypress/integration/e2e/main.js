f50a9d5f886826beaea9e5df312ee98fd2673f64
Alexander Philippov <alexphil@gmail.com>
2021-11-08 12:15:20 +0300

    Resolving comments

--- cypress/integration/e2e/main.js
+++ cypress/integration/e2e/main.js
@@ -1,24 +1,28 @@
 /// <reference types="cypress" />
+const siteUrl = 'http://127.0.0.1:3000'
 
 describe('E2E Tests', () => {
     it('Search, open details and close it after assert', () => {
         // Open site
-        cy.visit('http://127.0.0.1:3000')
+        cy.visit(siteUrl)
         // Assert redirect on search
-        cy.url().should('eq', 'http://127.0.0.1:3000/search')
+        cy.url().should('eq', `${siteUrl}/search`)
         cy.screenshot('search page', { overwrite: true })
         // Find Movie by name
         cy.get('#searchMovieInput').type('Black Panther')
         cy.get('.header__search > [type="button"]').click()
         // Assert query contains request
-        cy.url().should('include', '/search/All/Black%20Panther')
+        cy.url().should('eq', `${siteUrl}/search/All/Black%20Panther`)
         // Assert search result
         cy.get('.movie-list-header__total > span').contains(1)
         cy.screenshot('search result', { overwrite: true })
         // Opean Movie Details component
         cy.get('.movie-card__title').click()
         // Assert query contains movieId
-        cy.url().should('include', '/search/All/Black%20Panther?movie=')
+        cy.url().should(
+            'include',
+            `${siteUrl}/search/All/Black%20Panther?movie=`
+        )
         cy.screenshot('header with movie details', {
             overwrite: true,
         })
@@ -31,10 +35,7 @@ describe('E2E Tests', () => {
         // Close Movie details card
         cy.get('.header__search-icon').click()
         // Assert query does not contains movieId
-        cy.url().should(
-            'eq',
-            'http://127.0.0.1:3000/search/All/Black%20Panther'
-        )
+        cy.url().should('eq', `${siteUrl}/search/All/Black%20Panther`)
         cy.scrollTo(0, 0)
     })
 })
