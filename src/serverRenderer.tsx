3f5ed85549cd1e4122619ea1452ae8c45c75514e
Alexander Philippov <alexphil@gmail.com>
2021-11-16 16:11:21 +0300

    SSR node-fetch

--- src/serverRenderer.tsx
+++ src/serverRenderer.tsx
@@ -3,7 +3,19 @@ import ReactDOMServer from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom'
 import App from './App'
 import { Provider } from 'react-redux'
-import initialStore from './Store'
+import store from './Store'
+import { MoviesRoute, useCreateUrl } from 'Utils'
+import {
+    MoviesResponseModel,
+    SearchBy,
+    SortBy,
+    SortOrder,
+} from 'Components/MovieList/models'
+import fetch from 'node-fetch'
+import {
+    fetchMoviesError,
+    fetchMoviesSuccess,
+} from 'Components/MovieList/actions'
 
 const renderHtml = (html: string) => {
     return `
@@ -31,13 +43,44 @@ export default () => {
             send: (html: string) => void
         }
     ): void => {
-        const htmlString = ReactDOMServer.renderToString(
-            <StaticRouter location={req.url} context={{}}>
-                <Provider store={initialStore}>
-                    <App />
-                </Provider>
-            </StaticRouter>
-        )
-        res.send(renderHtml(htmlString))
+        const defaultLimit = '12'
+        const params: Record<string, string> = {
+            sortBy: SortBy.GENRE,
+            sortOrder: SortOrder.DESC,
+            search: '',
+            searchBy: SearchBy.TITLE,
+            filter: '',
+            limit: defaultLimit,
+            offset: '',
+        }
+        const apiUrl: string = useCreateUrl(MoviesRoute, params).toString()
+        fetch(apiUrl)
+            .then((response) => {
+                if (response.ok) {
+                    response.json().then((data) => {
+                        store.dispatch(
+                            fetchMoviesSuccess(data as MoviesResponseModel)
+                        )
+                        render(JSON.stringify(data))
+                    })
+                } else {
+                    store.dispatch(fetchMoviesError(response.statusText))
+                    render(response.statusText)
+                }
+            })
+            .catch((error) => {
+                store.dispatch(fetchMoviesError(error.toString()))
+                render(error.toString())
+            })
+        const render = (data: string) => {
+            const htmlString = ReactDOMServer.renderToString(
+                <StaticRouter location={req.url}>
+                    <Provider store={store}>
+                        <App />
+                    </Provider>
+                </StaticRouter>
+            )
+            res.send(renderHtml(htmlString))
+        }
     }
 }
