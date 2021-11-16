fe53babecafb5b58ba9e370b31a512312697de9b
Alexander Philippov <alexphil@gmail.com>
2021-11-16 23:49:16 +0300

    Try optimize run build

--- src/Components/MovieList/index.tsx
+++ src/Components/MovieList/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useLocation, useParams } from 'react-router-dom'
